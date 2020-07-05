178f112a811f8140e3f80c78aa95380b1d9f8408
Arnaud Picard <apc@emakina.fr>
2020-07-05 11:15:11 +0200

    N/A: Init navigation

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
 import Login from './Components/Login';
+import Search from './Components/Search';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
-        <Login/>
+        <NavigationContainer initialRouteName="Login">
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Search" component={Search} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
